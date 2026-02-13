dba05a5d1ece3c0241a99141efab143add4067df
abids <abids@debian.abids>
2026-02-13 08:41:30 +0530

    stage 12 done

--- xfs-interface/run.sh
+++ xfs-interface/run.sh
@@ -1,4 +1,5 @@
 load --int=7 ../spl/spl_progs/sample_int7.xsm
 load --os ../spl/spl_progs/os_startup.xsm
-load --init ../expl/expl_progs/upto20.xsm
+load --init ../expl/samples/numbers.xsm
 load --int=timer ../spl/spl_progs/sample_timer.xsm
+load --idle ../expl/expl_progs/idle.xsm
